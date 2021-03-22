eb171fee2418c1cda09dda73cc19610456790e86
Alain Rusnov <alainrusnov@gmail.com>
2021-03-22 10:54:40 -0400

    init push PoC off cases in deck.gl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DeckGL from "deck.gl";
+import { StaticMap } from 'react-map-gl';
+import axios from "axios";
+import { RenderLayers } from "./deck.gl-layer.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoieW9zbyIsImEiOiJja2d3aHVkMWUwYWZoMzFwbnNxdnhjbmtoIn0.72451mV-JRRnWfaqmz0ZnQ";
+const mapStyle = "mapbox://styles/mapbox/dark-v10";
+const INITIAL_VIEW_STATE = {
+  longitude: 12.8333,
+  latitude: 42.8333,
+  zoom: 4,
+  maxZoom: 20,
+  minZoom: 4,
+  pitch: 60,
+  bearing: 5
+};
+
+// let data;
+
+export default class App extends React.Component {
+  state = {};
+  constructor(props) {
+    super();
+    this.state = {
+      data: [],
+      hover: {
+        x: 0,
+        y: 0,
+        hoveredObject: null
+      }
+    };
+  }
+  renderTooltip({ x, y, object, layer }) {
+    this.setState({ hover: { x, y, layer, hoveredObject: object } });
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    axios.all([
+      axios.get('https://disease.sh/v2/countries?allowNull=false'),
+    ]).then(axios.spread((World) => {
+      let data = World.data || [];
+      data = data.map(function (location) {
+        return {
+          active: location.active,
+          country: location.country,
+          continent: location.continent,
+          coordinates: [location.countryInfo.long, location.countryInfo.lat]
+        };
+      });
+      // data = data.filter(location => (location.continent === "Europe"));
+      this.setState({ data: data });
+    })).catch((error) => {
+      console.log(error); return [];
+    })
+  }
+  render() {
+    const { hover, data } = this.state;
+    console.log(data);
+    return (
+      <div>
+        {hover.hoveredObject && (
+          <div style={{
+            position: "absolute",
+            zIndex: 1000,
+            background: "#ffffff",
+            pointerEvents: "none",
+            borderRadius: "5px",
+            left: hover.x,
+            top: hover.y
+          }} >
+            <ul className="hoveredObjectData">
+              <li><h4>{hover.hoveredObject.country}</h4></li>
+              <li>active cases: <span>{hover.hoveredObject.active.toLocaleString()}</span></li>
+            </ul>
+          </div>
+          )
+        }
+        <DeckGL layers={RenderLayers({ data: data, onHover: hover => this.renderTooltip(hover)})} initialViewState={INITIAL_VIEW_STATE} controller={true} ><StaticMap mapStyle={mapStyle} mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+        </DeckGL>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/deck.gl-layer.jsx
@@ -0,0 +1,28 @@
+import { ColumnLayer } from "deck.gl";
+import { scaleLinear } from "d3-scale";
+
+export const RenderLayers = (props) => {
+  let maxActive, minActive;
+  const radiusColumns = 20000;
+  const { data, onHover } = props;
+  const value = data.map((a) => a.active);
+  maxActive = Math.max(...value);
+  minActive = Math.min(...value);
+  const elevation = scaleLinear([minActive, maxActive], [1000, 20000]);
+
+  return [
+    new ColumnLayer({
+    id: "cases",
+    data,
+    pickable: true,
+    extruded: true,
+    getPosition: d => d.coordinates,
+    diskResolution: 10,
+    radius: radiusColumns,
+    elevationScale: 50,
+    getFillColor: [138, 3, 3],
+    getElevation: d => elevation(d.active),
+    onHover,
+    }),
+  ];
+}
