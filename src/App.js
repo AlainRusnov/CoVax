9bf88bcef742bf9606eca73537ef8954f5ff81d5
Alain Rusnov <alainrusnov@gmail.com>
2021-03-23 17:03:15 -0400

    data merge debug

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DeckGL from "deck.gl";
 import { StaticMap } from 'react-map-gl';
 import axios from "axios";
 import { RenderLayers } from "./deck.gl-layer.jsx";
+import _ from "lodash";
 
 
 const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoieW9zbyIsImEiOiJja2d3aHVkMWUwYWZoMzFwbnNxdnhjbmtoIn0.72451mV-JRRnWfaqmz0ZnQ";
@@ -40,19 +41,21 @@ export default class App extends React.Component {
   }
   fetchData() {
     axios.all([
-      axios.get('https://disease.sh/v2/countries?allowNull=false'),
-      axios.get('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json'),
+      axios.get('https://disease.sh/v3/covid-19/countries?allowNull=false'),
+      axios.get('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json'), // https://disease.sh/v3/covid-19/vaccine/coverage/countries?lastdays=1 //https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/vaccine_data_global.csv
     ]).then(axios.spread((World, vax) => {
 
-      // let vaxData = vax.data || [];
-      // // data = vaxData;
-      // // console.log(vaxData);   // FRA.data.total_vaccinations
+      let vaxData = vax.data || [];
+      // // data = vaxData
+      // console.log(vaxData);   // FRA.data.total_vaccinations
 
-      // vaxData = vaxData.map(function (vaxData) {
-      //   return {
-      //     vaxCountry: vaxData.country.data,  // data.total_vaccinations,
-      //   };
-      // });
+      vaxData = vaxData.map(function (location) {
+        return {
+          // country: location.country,  // data.total_vaccinations,
+          vaxCount: location.data ? (location.data).pop() : "N/A",
+          iso: location.iso_code,
+        };
+      });
 
 
       let WorldData = World.data || [];
@@ -68,13 +71,20 @@ export default class App extends React.Component {
           clickable: true,
           iso: location.countryInfo.iso3,
           deaths: location.deaths,
+          population: location.population,
           updated: new Date(location.updated).toISOString().substring(0, 10),
           recovered: location.recovered ? location.recovered : "N/A",
         };
 
       });
       // data = data.filter(location => (location.continent === "Europe"));
-      // data = data.concat(vaxData);
+      // data = WorldData.concat(vaxData);
+      // data = Object.assign(vaxData, WorldData);
+      // data = {...WorldData, ...vaxData}
+      // data = _.merge(WorldData, vaxData);
+      // var merged = _.merge(_.keyBy(WorldData, 'iso'), _.keyBy(vaxData, 'iso'));
+      // var data = _.values(merged);
+      // console.log(data);
       this.setState({ data: data });
     })).catch((error) => {
       console.log(error); return [];
@@ -82,9 +92,10 @@ export default class App extends React.Component {
   }
   render() {
     const { hover, data } = this.state;
-    console.log(data);
+    // console.log(data);
     return (
       <div>
+        <h1>CoVax</h1>
         {hover.hoveredObject && (
           <div style={{
             position: "absolute",
@@ -101,6 +112,7 @@ export default class App extends React.Component {
               <li>Active cases: <span>{hover.hoveredObject.active.toLocaleString()}</span></li>
               <li>Recovered: <span>{hover.hoveredObject.recovered.toLocaleString()}</span></li>
               <li>Deaths: <span>{hover.hoveredObject.deaths.toLocaleString()}</span></li>
+              <li>Population: <span>{hover.hoveredObject.population.toLocaleString()}</span></li>
               <li>updated: <span>{hover.hoveredObject.updated}</span></li>
             </ul>
           </div>
@@ -109,6 +121,7 @@ export default class App extends React.Component {
         <DeckGL layers={RenderLayers({ data: data, onHover: hover => this.renderTooltip(hover)})} initialViewState={INITIAL_VIEW_STATE} controller={true} ><StaticMap mapStyle={mapStyle} mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
         </DeckGL>
       </div>
+
     );
   }
 }
--- src/deck.gl-layer.jsx
+++ src/deck.gl-layer.jsx
@@ -1,9 +1,9 @@
-import { ColumnLayer } from "deck.gl";
-import { scaleLinear } from "d3-scale";
+import { ColumnLayer, HexagonLayer } from "deck.gl";
+import { scaleLinear, scaleLog } from "d3-scale";
 
 export const RenderLayers = (props) => {
   let maxActive, minActive;
-  const radiusColumns = 30000;
+  const radiusColumns = 40000;
   const { data, onHover } = props;
   const value = data.map((a) => a.active);
   maxActive = Math.max(...value);
@@ -12,15 +12,19 @@ export const RenderLayers = (props) => {
 
   return [
     new ColumnLayer({
-    id: "cases",
+    id: "situation",
     data,
     pickable: true,
     extruded: true,
     getPosition: d => d.coordinates,
-    diskResolution: 10,
+    diskResolution: 100,
     radius: radiusColumns,
     elevationScale: 100,
-    getFillColor: [138, 3, 3],
+    // wireframe: true,
+    // getLineColor: [0, 0, 0],
+    // getLineWidth: 20,
+    // colorRange: true,
+    getFillColor: [127, 3, 3, 255],  ///[48, 128, (d.population / d.active) * 100 * 255, 255],    ///[(d.population / d.active) * 100 * 255, 3, 3],
     getElevation: d => elevation(d.active),
     onHover,
     }),
